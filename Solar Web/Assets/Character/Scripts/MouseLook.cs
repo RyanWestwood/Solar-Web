22b54a52e97767fd27092390dcfc4df21d4af307
shakir12345 <55580647+shakir12345@users.noreply.github.com>
2021-03-14 17:26:30 +0000

    movement with animation has been implemented

---
+++ Solar Web/Assets/Character/Scripts/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f; //control the speed of the mouse
+
+    public Transform player; // reference to player object
+
+    float xRotation = 0f; // variable to rotate around x axis
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // allows cursor to not be shown when playing the game and also locks the cursor to the middle of the screen.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; // gets input from mouse movement on the x axis.
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; // gets input from mouse movement on the y axis.
+
+        xRotation -= mouseY; // decrease xrotation based on mouse movement on the y axis
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // mathf.clamp allows the user to set min and max rotation of mouse movement.
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //allows rotation in the x axis, Quaternion is responsible for rotation in unity.
+        player.Rotate(Vector3.up * mouseX); // player rotates in the x axis when mouse movement on the x axis occurs.
+    }
+}
---
+++ Solar Web/Assets/Character/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public CharacterController controller; //reference to character controller
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public Animator anim; //reference to animator
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetInteger("condition", 1); //if W key is pressed then the condition changes from 0 to 1 and the character will move forward in world space whilst the walk animation is played.
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("condition", 0); //if W is no longer being pressed then the idle animation plays.
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            anim.SetInteger("condition", 2); //if W key is pressed then the condition changes from 0 to 2 and the character will run forward in world space whilst the run animation is played.
+            transform.position += transform.forward * runSpeed * Time.deltaTime;
+
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            anim.SetInteger("condition", 0); //if Q is no longer being pressed then the idle animation plays.
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetInteger("condition", 3);  //if space key is pressed then the condition changes from 0 to 3 and the character will jump. note: will have to hold space key for the full jump animation to play out.
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetInteger("condition", 0); //if space key is no longer being pressed then the idle animation plays.
+        }
+
+    }
+}
